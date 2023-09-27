082dfb2b83e6a05a065db6ef805bcb6ce0c948f8
Chalat Srisuwan <sakichan2521@gmail.com>
2023-09-27 19:42:34 +0700

    The Player can pick up weapons.

--- Source/Pangaea/Weapon/Weapon.cpp
+++ Source/Pangaea/Weapon/Weapon.cpp
@@ -4,6 +4,7 @@
 #include "Pangaea/Weapon/Weapon.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Pangaea/Player/PlayerAvatar.h"
 // Sets default values
 AWeapon::AWeapon()
 {
@@ -20,13 +21,54 @@ AWeapon::AWeapon()
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+	OnActorBeginOverlap.AddDynamic(this, &AWeapon::OnWeaponBeginOverlap);
+}
+
+void AWeapon::OnWeaponBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	UE_LOG(LogTemp, Log, TEXT("Weapon overlapped"));
+	auto OtherCharacter = Cast<ACharacter>(OtherActor);
+	if(OtherCharacter == nullptr)
+	{
+		return;
+	}
+	
+	 if(Holder == nullptr)
+	{
+		auto const PlayerAvatar = Cast<APlayerAvatar>(OtherCharacter);
+	 	if(PlayerAvatar != nullptr)
+	 	{
+	 		Holder = OtherCharacter;
+	 		TArray<AActor*> AttachedActors;
+	 		OtherActor->GetAttachedActors(AttachedActors, true);
+	 		for(int i = 0; i < AttachedActors.Num(); ++i)
+	 		{
+	 			AttachedActors[i]->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+	 			AttachedActors[i]->SetActorRotation(FQuat::Identity);
+	 			AWeapon* Weapon = Cast<AWeapon>(AttachedActors[i]);
+	 			Weapon->Holder = nullptr;
+	 		}
+	 		AttachToComponent(Holder->GetMesh(),FAttachmentTransformRules::SnapToTargetIncludingScale, FName("hand_rSocket"));
+	 	}
+	}
+	// else if(bWithInAttackRange(0.f, OtherActor))
+	// {
+	// 	//
+	// }
+	
 	
 }
+
+
 
 // Called every frame
 void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if(Holder == nullptr)
+	{
+		FQuat rotQuat = FQuat(FRotator(0.f, 300.f * DeltaTime, 0.f));
+	}
 }
+
 
--- Source/Pangaea/Weapon/Weapon.h
+++ Source/Pangaea/Weapon/Weapon.h
@@ -19,7 +19,8 @@ class PANGAEA_API AWeapon : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AWeapon();
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite) ACharacter* Holder = nullptr;
+	UPROPERTY(EditAnywhere, Category = "Weapon|Params") float Strength = 10;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -28,6 +29,9 @@ protected:
 
 	UPROPERTY(EditAnywhere, Category = "Weapon|Mesh")
 	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+
+	UFUNCTION() void OnWeaponBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	bool bWithInAttackRange(float AttackRange, AActor* Target);
 
 public:	
 	// Called every frame
